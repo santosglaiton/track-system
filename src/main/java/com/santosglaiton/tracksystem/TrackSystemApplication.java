5006ad62b94176c03ab0fde082c801ae2b106ff6
glaiton.santos <glaitonlemos@gmail.com>
2021-01-18 12:03:59 -0300

    First commit

---
+++ src/main/java/com/santosglaiton/tracksystem/TrackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.santosglaiton.tracksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosglaiton/tracksystem/TrackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosglaiton.tracksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
